0cf7f226edfd32ba33adc500c4cad82e1f2cbf91
animusanima <molockokannzocko@gmail.com>
2025-12-03 18:26:37 +0100

    Started to solve task for Day two
    - Reading input data
    - Printing numbers to see similarities
    - First approach: comparing numbers with in a block of two numbers

--- src/main/java/de/animusanima/advent/of/code/days/two/DayTwo.java
+++ src/main/java/de/animusanima/advent/of/code/days/two/DayTwo.java
@@ -3,6 +3,8 @@ package de.animusanima.advent.of.code.days.two;
 import de.animusanima.advent.of.code.utils.InputFileReader;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,7 +24,25 @@ public class DayTwo {
             data = Collections.emptyList();
         }
 
-        data.stream().forEach(System.out::println);
+        List<String> invalidIds = new ArrayList<>();
+        for (String numbers : data) {
+            String[] ids = numbers.split("-");
+
+            System.out.println(Arrays.toString(ids));
+
+            String idOne = ids[0];
+            String idTwo = ids[1];
+            System.out.println("ID One " + idOne);
+            System.out.println("ID Two " + idTwo);
+            for (int i = 0; i < idOne.length(); i += 2) {
+                String partOne = i + 2 < idOne.length() ? idOne.substring(i, i + 2) : idOne.substring(i);
+                String partTwo = i + 2 < idTwo.length() ? idTwo.substring(i, i + 2) : idTwo.substring(i);
+                if (partOne.equals(partTwo)) {
+                    invalidIds.add(partOne);
+                }
+            }
+        }
+        System.out.println(invalidIds);
 
     }
 
