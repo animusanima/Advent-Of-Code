d00c8d3cfeeab4fbad3c71dbc8422b0749be59d9
animusanima <molockokannzocko@gmail.com>
2025-12-14 13:45:59 +0100

    Removed Import from InvalidIDFilter

--- src/main/java/de/animusanima/advent/of/code/days/two/utils/InvalidIDFilter.java
+++ src/main/java/de/animusanima/advent/of/code/days/two/utils/InvalidIDFilter.java
@@ -1,7 +1,6 @@
 package de.animusanima.advent.of.code.days.two.utils;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
