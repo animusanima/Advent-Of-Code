2dd3660ba39637f16427f42118ecc34c41289b29
animusanima <molockokannzocko@gmail.com>
2025-12-18 21:27:15 +0100

    Removed unused imports from InvalidIDFilterTest

--- src/test/java/de/animusanima/advent/of/code/days/two/utils/InvalidIDFilterTest.java
+++ src/test/java/de/animusanima/advent/of/code/days/two/utils/InvalidIDFilterTest.java
@@ -1,14 +1,11 @@
 package de.animusanima.advent.of.code.days.two.utils;
 
-import org.assertj.core.api.Assertions;
-import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class InvalidIDFilterTest {
 
